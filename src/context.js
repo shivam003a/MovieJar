4367e854bafcc9612dd29409c20f4f8f1afc8066
shivam003a <shivam003a@gmail.com>
2023-02-01 00:22:27 +0530

    random movie on homescreen

--- src/context.js
+++ src/context.js
@@ -3,13 +3,18 @@ import { createContext, useEffect, useState } from "react";
 const AppContext = createContext();
 const API = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_KEY}`
 
+const sampleMovie = ["Iron Man", "Hulk", "Captain America", "Harry Potter", "Fast Fourious", "James Bond","Spider Man","Transformers","Batman", "Ice Age","Batman", "Toy Story","Avengers","Die Hard", "Resident Evil","Alien","Jack Ryan","Rocky"];
 
 
 const AppProvider = ({ children }) => {
+
+    const rIndex = Math.floor(Math.random()*sampleMovie.length);
+    const rMovie = sampleMovie[rIndex];
+
     const [isLoading, setIsLoading] = useState(true);
     const [movieList, setMovieList] = useState([]);
     const [isError, setIsError] = useState({ show: "false", msg: "" })
-    const [query, setQuery] = useState('Iron Man');
+    const [query, setQuery] = useState(rMovie);
     const [page, setPage] = useState(1);
     const [totalPage, setTotalPage] = useState(1);
 
